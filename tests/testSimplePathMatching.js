a00ece8fc511e8bb3ce2e39b7a849241b3f1dd1d
Vlad Balmos <vlad.balmos@code932.com>
2020-03-31 13:17:55 +0300

    Add tests

---
+++ tests/testSimplePathMatching.js
@@ -0,0 +1,20 @@
+require("../../../psknode/bundles/testsRuntime");
+
+const dc = require("double-check");
+const assert = dc.assert;
+
+const middleware = require("../");
+const utils = require('./utils');
+
+assert.callback('Middleware simple path matching', (done) => {
+    const server = middleware.getMiddleware();
+    const request = utils.createRequest({
+        url: 'http://localhost/test'
+    });
+    const response = utils.createResponse();
+
+    server.use('/test', (req, res) => {
+        done();
+    })
+    server.executeRequest(request, response);
+});
